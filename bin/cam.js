23cd5baf3701d990ef27f0477a63aa69dccdc60c
ChenJian <toughc@qq.com>
2024-11-18 23:09:06 +0800

    feat: clone project

--- bin/cam.js
+++ bin/cam.js
@@ -1,2 +1,11 @@
 #! /usr/bin/env node
+import { Command } from "commander";
+import { create } from "../lib/create.js";
 
+const program = new Command();
+program.command('create <project-name>')
+.description('create a new project')
+.action(function (projectName, options) {
+  create(projectName)
+})
+program.parse();
